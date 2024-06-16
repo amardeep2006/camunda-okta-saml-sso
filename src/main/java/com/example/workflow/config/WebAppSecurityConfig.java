65ac0ba532f606726a869e3cde40063b23be3f80
amardeep2006 <amardeep2006@gmail.com>
2024-06-16 20:19:35 +0530

    feat(7.21): upgraded springboot to 3.3.0 and camunda 7.21

--- src/main/java/com/example/workflow/config/WebAppSecurityConfig.java
+++ src/main/java/com/example/workflow/config/WebAppSecurityConfig.java
@@ -1,89 +1,115 @@
 package com.example.workflow.config;
 
 import com.example.workflow.filter.webapp.WebAppAuthenticationProvider;
+import jakarta.inject.Inject;
+import org.camunda.bpm.spring.boot.starter.property.CamundaBpmProperties;
 import org.camunda.bpm.webapp.impl.security.auth.ContainerBasedAuthenticationFilter;
 import org.opensaml.saml.saml2.core.Assertion;
 import org.opensaml.saml.saml2.core.Attribute;
 import org.opensaml.saml.saml2.core.AttributeStatement;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.saml2.provider.service.authentication.OpenSamlAuthenticationProvider;
+import org.springframework.security.saml2.provider.service.authentication.OpenSaml4AuthenticationProvider;
+import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
+import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
+import org.springframework.security.saml2.provider.service.web.authentication.Saml2WebSsoAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
 @Configuration
 public class WebAppSecurityConfig {
 
-    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
-    GrantedAuthoritiesMapper authoritiesMapper = (authCol -> authCol);
-    // Actual code to extract the Authorities (or roles) from the Assertion
-    Converter<Assertion, Collection<? extends GrantedAuthority>> authoritiesExtractor = assertion -> {
+//    @Value("${sso.enable.singlelogout}")
+//    private boolean singleLogout;
+//    private final CustomLogoutSuccessHandler customLogoutSuccessHandler;
+//    public WebAppSecurityConfig(CustomLogoutSuccessHandler customLogoutSuccessHandler) {
+//        this.customLogoutSuccessHandler = customLogoutSuccessHandler;
+//    }
+    @Autowired
+    RelyingPartyRegistrationRepository relyingPartyRegistrationRepository;
+    @Inject
+    private CamundaBpmProperties camundaBpmProperties;
 
-        List<SimpleGrantedAuthority> userRoles
-                = assertion.getAttributeStatements().stream()
-                .map(AttributeStatement::getAttributes)
-                .flatMap(Collection::stream)
-                .filter(attr -> "groups".equalsIgnoreCase(attr.getName()))
-                .map(Attribute::getAttributeValues)
-                .flatMap(Collection::stream)
-                .map(xml -> new SimpleGrantedAuthority("ROLE_" + xml.getDOM().getTextContent()))
-                .toList();
-        return userRoles;
-    };
-    @Value("${sso.enable.singlelogout}")
-    private boolean singleLogout;
-    public WebAppSecurityConfig(CustomLogoutSuccessHandler customLogoutSuccessHandler) {
-        this.customLogoutSuccessHandler = customLogoutSuccessHandler;
-    }
-
-    @SuppressWarnings("deprecation") //OpenSaml4 jars are not available by default in Spring security
+    // change : Return a Spring Bean SecurityFilterChain
     @Bean
-    public SecurityFilterChain webAppFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf().ignoringAntMatchers("/engine-rest/**", "/camunda/api/**")
-                .and()
-                .authorizeRequests()
-                .antMatchers("/swaggerui/**", "/engine-rest/**", "/error/**")
-                .permitAll()
-                .and()
-                .antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/camunda/**")
-                .authenticated()
-                .anyRequest()
-                .permitAll()
-                .and()
-                .saml2Login()
-                .addObjectPostProcessor(new ObjectPostProcessor<OpenSamlAuthenticationProvider>() {
-                    public <P extends OpenSamlAuthenticationProvider> P postProcess(
-                            P samlAuthProvider) {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-                        // Set the Authorities extractor
-                        samlAuthProvider.setAuthoritiesExtractor(authoritiesExtractor);
-                        samlAuthProvider.setAuthoritiesMapper(authoritiesMapper);
-                        return samlAuthProvider;
-                    }
-                });
+        // Create a filter to handle generation of SAML Metadata file
+        Saml2MetadataFilter filter = new Saml2MetadataFilter(
+                new DefaultRelyingPartyRegistrationResolver(this.relyingPartyRegistrationRepository),
+                new OpenSamlMetadataResolver());
 
-        if (singleLogout) {
-            http
-                    .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/**/logout"))
-                    .logoutSuccessHandler(customLogoutSuccessHandler);
+        // Actual code to extract the Authorities (or roles) from the Assertion
+        Converter<Assertion, Collection<? extends GrantedAuthority>> authoritiesExtractor = assertion -> {
 
-        }
+            List<SimpleGrantedAuthority> userRoles
+                    = assertion.getAttributeStatements()
+                    .stream()
+                    .map(AttributeStatement::getAttributes)
+                    .flatMap(Collection::stream)
+                    .filter(attr -> "groups".equalsIgnoreCase(attr.getName()))
+                    .map(Attribute::getAttributeValues)
+                    .flatMap(Collection::stream)
+                    .map(xml -> new SimpleGrantedAuthority("ROLE_" + xml.getDOM()
+                            .getTextContent()))
+                    .toList();
+            return userRoles;
+        };
+
+        // change : Create a version of SAML Authenticator Provider which will convert the
+        // "groups" claim into Authorities in the SAML 2 Authentication object
+        Converter<OpenSaml4AuthenticationProvider.ResponseToken, Saml2Authentication> authConvertor
+                = OpenSaml4AuthenticationProvider.createDefaultResponseAuthenticationConverter();
+        OpenSaml4AuthenticationProvider samlAuthProv = new OpenSaml4AuthenticationProvider();
+        samlAuthProv.setResponseAuthenticationConverter((responseToken) -> {
+
+            // Make sure the authorities are set in the SAML Authentication
+            Saml2Authentication authentication = authConvertor.convert(responseToken);
+            Assertion assertion = responseToken.getResponse()
+                    .getAssertions()
+                    .get(0);
+            AuthenticatedPrincipal principal = (AuthenticatedPrincipal) authentication.getPrincipal();
+            Collection<? extends GrantedAuthority> authorities = authoritiesExtractor.convert(assertion);
+            return new Saml2Authentication(principal, authentication.getSaml2Response(), authorities);
+        });
+
+        String webAppPath = camundaBpmProperties.getWebapp()
+                .getApplicationPath(); //default value for root-context is /camunda
+
+        http.csrf(csrf -> csrf.ignoringRequestMatchers(antMatcher(webAppPath + "/api/**")))
+                .saml2Login(withDefaults())
+//                .saml2Logout(withDefaults())
+                .authenticationProvider(samlAuthProv) // change : register the new SAML Auth provider
+                .addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class)
+                .authorizeHttpRequests(authorize ->
+                                               authorize
+                                                       .requestMatchers(antMatcher("/swaggerui/**"))
+                                                       .permitAll()
+                                                       .requestMatchers(antMatcher("/logout"))
+                                                       .permitAll()
+                                                       .requestMatchers(antMatcher(webAppPath + "/**"))
+                                                       .authenticated()
+                                                       .anyRequest()
+                                                       .permitAll());
+
+        // change : return the SecurityFilterChain
         return http.build();
     }
 
@@ -97,5 +123,4 @@ public class WebAppSecurityConfig {
         filterRegistration.addUrlPatterns("/camunda/app/*");
         return filterRegistration;
     }
-
 }
